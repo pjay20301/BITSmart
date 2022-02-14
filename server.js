e00bd2c4273c54fa668fada403fcb8a90d77a73a
Jay Patel <pjay20301@gmail.com>
2022-02-14 11:22:02 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+
+dotenv.config({ path: 'config.env' })
+const port = process.env.PORT
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+  res.send('Server running')
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port https://localhost:${port}`)
+})
