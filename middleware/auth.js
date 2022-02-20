478642cb8f921ec2c00df644ca52b35de816cf06
Jay Patel <pjay20301@gmail.com>
2022-02-20 16:45:44 +0530

    tokens generated

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require('../model/user.js')
+
+const jwtsecret = process.env.JWTSECRET
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, jwtsecret)
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        if (!user) {
+        throw new Error('User not found')
+        }
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(405).send(e)
+    }
+}
+
+module.exports = auth
