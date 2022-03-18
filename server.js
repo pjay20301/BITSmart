b04183d1149e0921ae1986b36c9f61a18b348cb0
Jay Patel <pjay20301@gmail.com>
2022-03-18 15:59:34 +0530

    one more commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB()
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'))
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 }
