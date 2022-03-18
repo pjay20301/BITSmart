11fe5ef1c3bbde314cf54758192bf737f41dea59
Jay Patel <pjay20301@gmail.com>
2022-03-18 16:10:21 +0530

    path of index.html updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB()
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve('./client/public/index.html'))
     })
 }
 
