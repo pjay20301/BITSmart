a61dae04aba246c3331ada1bd513528db9b49654
Jay Patel <pjay20301@gmail.com>
2022-03-19 11:50:58 +0530

    script changed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
 app.use('/', require('./routes/router'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/static', express.static(path.resolve("./client/build")))
+    app.use(express.static(path.resolve("./client/build")))
     // app.get('*', (req, res) => {
     //     res.sendFile(path.resolve('./client/src/index.js'))
     // })
