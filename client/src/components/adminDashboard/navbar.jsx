884028f2e5eb23db536f7bc03545c070a7e661ce
Durba Satpathi <70879852+durba-s@users.noreply.github.com>
2022-04-09 06:56:40 +0530

    admin dashboard-vendor auth frontend done

---
+++ client/src/components/adminDashboard/navbar.jsx
@@ -0,0 +1,99 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import './navbar.css';
+import "react-pro-sidebar/dist/css/styles.css";
+import { useSelector, useDispatch } from 'react-redux';
+import { useAlert } from 'react-alert';
+import navBar from './navbar';
+import axios from 'axios';
+import { Link,useNavigate } from 'react-router-dom';
+import {ProSidebar,Menu,MenuItem,SidebarHeader,SidebarFooter,SidebarContent,} from "react-pro-sidebar";
+import { FiHome, FiLogOut} from "react-icons/fi";
+import { RiShoppingCart2Line } from "react-icons/ri";
+import { AiOutlineShop,AiOutlineMenuFold,AiOutlineMenuUnfold } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import {ImHistory} from "react-icons/im";
+import logo from './logo-crop.png';
+import {MdAddShoppingCart,MdOutlineRateReview} from "react-icons/md";
+import {BsGraphUp} from "react-icons/bs";
+
+
+const NavBar = () => {
+        //create initial menuCollapse state using useState hook
+    const [menuCollapse, setMenuCollapse] = useState(false)
+
+    //create a custom function that will change menucollapse state from false to true and true to false
+    const menuIconClick = () => {
+        //condition checking to change state from true to false and vice versa
+        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(false);
+      };
+    const menuItemClick = () =>{
+
+    };
+    return (
+        <>
+            <div id='header'>
+                {/* collapsed props to change menu size using menucollapse state */}
+                <ProSidebar collapsed={menuCollapse}>
+                    <SidebarHeader>
+                        <div className='closemenu' onClick={menuIconClick}>
+                            {/* changing menu collapse icon on click */}
+                            {/* {menuCollapse ? (
+                  <AiOutlineMenuUnfold/>
+                ) : (
+                  <AiOutlineMenuFold/>
+                )} */}
+                        </div>
+                        <div className='logotext'>
+                            {/* small and big change using menucollapse state */}
+                            <p>{menuCollapse ? 'BM' : 'BITSmart - Admin'}</p>
+                            {menuCollapse ? (
+                                <img
+                                    src={logo}
+                                    alt='Ecommerce'
+                                    width='40'
+                                    height='30'
+                                />
+                            ) : (
+                                <img
+                                    src={logo}
+                                    alt='Ecommerce'
+                                    width='70'
+                                    height='50'
+                                />
+                            )}
+                        </div>
+                    </SidebarHeader>
+                    <SidebarContent>
+                        <Menu iconShape='square'>
+                                    <MenuItem icon={<FiHome />}>
+                                            Home
+                                            <Link to='/request'/>
+                                    </MenuItem>
+                                    <MenuItem icon={<MdAddShoppingCart />}>
+                                            Vendor Requests
+                                            <Link to='/request'/>
+                                    </MenuItem>
+                                    <MenuItem icon={<MdOutlineRateReview />}>
+                                        Complaints
+                                    </MenuItem>
+                                    <MenuItem icon={<AiOutlineShop />}>
+                                            Approved Vendors
+                                            <Link to='/request'/>
+                                    </MenuItem>
+                                    <MenuItem icon={<BsGraphUp />}>
+                                            Sales Statistics
+                                            <Link to='/request'/>
+                                    </MenuItem>
+                        </Menu>
+                    </SidebarContent>
+                    <SidebarFooter>
+                        <Menu iconShape='square'>
+                            <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+                        </Menu>
+                    </SidebarFooter>
+                </ProSidebar>
+            </div>
+        </>
+    )
+}
+export default NavBar
--- client/src/components/adminDashboard/request.jsx
+++ client/src/components/adminDashboard/request.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 import RequestCard from './requestCard';
+import { CgProfile } from "react-icons/cg";
+import './request.css';
+import NavBar from './navbar';
 const url = 'http://localhost:5000/'
 const Request = () => {
 
@@ -19,13 +24,16 @@ const Request = () => {
                 <p>Welcome to Ecommerce</p>
                 <h1>FIND AMAZING PRODUCTS BELOW</h1>
             </div> */}
-                {/* <SideBar /> */}
-                <h2 className='homeHeading'>Pending Requests</h2>
-                    <h1>Requests</h1>
+            <div className='ad-container'>
+                <NavBar />
+                <div className='admin-box'>
+                    <h3>Pending Vendor Requests</h3>
                     {requests &&
                         requests.map((vendor) => (
                             <RequestCard key={vendor._id} request={vendor} />
                         ))}
+                </div>
+                </div>
 
         </>
     )
